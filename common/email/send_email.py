0cc6c9058993d026b45eb1a2b0b57216602e3707
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2025-04-13 02:10:32 +0800

    邮件发送工具类

--- common/email/send_email.py
+++ common/email/send_email.py
@@ -9,115 +9,142 @@
 @Version 0.0.1
 @Description None
 """
-import logging
-import os
-import smtplib
-import sys
-# 负责构造文本
+import mimetypes
+from pathlib import Path
+from typing import List, Union
 from email.mime.text import MIMEText
-# 负责构造图片
 from email.mime.image import MIMEImage
-# 负责将多个对象集合起来
+from email.mime.audio import MIMEAudio
+from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
+from smtplib import SMTP, SMTP_SSL
+from common.log import Logger
+from config.read_config import ReadConfig
 
-from common.logging_log import MyLogger
-from common.read_config import ReadConfig
-
-conf = ReadConfig()
-# 日志设置
-log = MyLogger(tag='ballLog', colorful=True, save_pth="../../logs", existing_counts=7)
+logger = Logger(name="email").get_logger()
 
 
-class SendEmail(object):
-    """
-    :param receivers: 收件人，可设置多个收件人：abc@yahoo.com.a,cba@yahoo.com
-    :param theme: 主题
-    :param content: 内容
-    """
-
-    def __init__(self, receivers: str, theme: str, content: str):
-        super().__init__()
-        self.mm = MIMEMultipart('related')
-        # 创建SMTP对象
-        self.stp = smtplib.SMTP()
-        self.mail_receivers = receivers
-        self.email_theme = theme
-        self.email_content = content
-
-    def format_email(self, send_email):
+class SendEmail:
+    """邮件发送类"""
+    
+    def __init__(self, use_ssl: bool = True):
         """
-        格式化邮件
-        :return:
+        初始化邮件发送类
+        :param host: SMTP服务器地址
+        :param port: SMTP服务器端口
+        :param sender: 发件人邮箱
+        :param password: 邮箱授权码
+        :param use_ssl: 是否使用SSL连接，默认True
         """
-        new_email = ""
-        if "," in send_email:
-            for rows in send_email.split(","):
-                new_email = new_email + (rows.split("@")[0] + "<%s>" % rows + ",")
-            send_email = new_email[0:-1]
-        else:
-            send_email = send_email.split("@")[0] + "<%s>" % send_email
-        return send_email
+        self.conf = ReadConfig()
+        self.host = self.conf.mail_host
+        self.port = self.conf.mail_port
+        self.sender = self.conf.mail_sender
+        self.password = self.conf.mail_license
+        self.use_ssl = use_ssl
+        self.smtp = SMTP_SSL(self.host, self.port) if use_ssl else SMTP(self.host, self.port)
+        self.message = MIMEMultipart('alternative')
+        self._init_message()
 
-    def set_file(self, file_path):
-        """
-        设置文件
-        :return:
-        """
-        for rows in file_path.split(","):
-            file_name = rows.split("/")[len(rows.split("/")) - 1]
-            log.info("开始添加名为%s的附件" % file_name)
-            # 构造附件
-            attach = MIMEText(open(rows, 'rb').read(), 'base64', 'utf-8')
-            # 设置附件信息
-            attach["Content-Disposition"] = 'attachment; filename="%s"' % file_name
-            # 添加附件到邮件信息当中去
-            self.mm.attach(attach)
+    def _init_message(self):
+        """初始化邮件基本信息"""
+        self.message['From'] = self._format_address(self.sender)
+        self.message.preamble = 'You need a MIME-aware mail reader to see this message.'
 
-    def send(self):
+    def _format_address(self, address: str) -> str:
+        """格式化邮件地址"""
+        if '@' not in address:
+            raise ValueError(f"Invalid email address: {address}")
+        name, addr = address.split('@')
+        return f"{name}<{address}>"
+
+    def _get_mime_type(self, filepath: str) -> str:
+        """获取文件MIME类型"""
+        return mimetypes.guess_type(filepath)[0] or 'application/octet-stream'
+
+    def set_receivers(self, receivers: Union[str, List[str]]):
+        """设置收件人"""
+        if isinstance(receivers, str):
+            receivers = [receivers]
+        self.message['To'] = ','.join(self._format_address(r) for r in receivers)
+        self.receivers = receivers
+        return self
+
+    def set_subject(self, subject: str):
+        """设置邮件主题"""
+        self.message['Subject'] = Header(subject, 'utf-8')
+        return self
+
+    def set_content(self, content: str, content_type: str = 'plain'):
         """
-        发送邮件
-        :return:
+        设置邮件内容
+        :param content: 邮件内容
+        :param content_type: 内容类型，'plain' 或 'html'
+        """
+        self.message.attach(MIMEText(content, content_type, 'utf-8'))
+        return self
+
+    def add_attachment(self, filepath: Union[str, Path]):
+        """
+        添加附件
+        :param filepath: 文件路径
         """
         try:
-            if conf.mail_host is None or conf.mail_sender is None or conf.mail_license is None or self.mail_receivers is None:
-                log.error("邮箱配置错误！邮箱参数为 None")
-                sys.exit()
+            filepath = Path(filepath)
+            if not filepath.exists():
+                logger.error(f"附件不存在: {filepath}")
+                return self
 
-            log.info("开始发送邮件")
+            mime_type = self._get_mime_type(str(filepath))
+            with open(filepath, 'rb') as f:
+                data = f.read()
 
-            # 主题
-            theme = """%s""" % self.email_theme
-            # 设置发送者，格式：sender_name<******@163.com>
-            self.mm["From"] = self.format_email(conf.mail_sender)
-            self.mm["To"] = self.format_email(self.mail_receivers)
-            self.mm["Subject"] = Header(theme, "utf-8")
-            log.info("电子邮件接收者有: %s" % self.format_email(self.mail_receivers))
+            if mime_type.startswith('image/'):
+                attachment = MIMEImage(data)
+            elif mime_type.startswith('audio/'):
+                attachment = MIMEAudio(data)
+            else:
+                attachment = MIMEApplication(data)
 
-            # 正文
-            content = """%s""" % self.email_content
-            send_text = MIMEText(content, "plain", "utf-8")
-            self.mm.attach(send_text)
-
-            # # 附件
-            # if self.attachments is not None:
-            #     self.set_file(self.attachments)
-            # 设置发件人邮箱的域名和端口，端口地址为25
-            self.stp.connect(conf.mail_host, 25)
-            # set_debuglevel(1)可以打印出和SMTP服务器交互的所有信息
-            # stp.set_debuglevel(1)
-            # 登录邮箱，传递参数1：邮箱地址，参数2：邮箱授权码
-            self.stp.login(conf.mail_sender, conf.mail_license)
-            # 发送邮件，传递参数1：发件人邮箱地址，参数2：收件人邮箱地址，参数3：把邮件内容格式改为str
-            self.stp.sendmail(conf.mail_sender, self.mail_receivers, self.mm.as_string())
-            log.info("邮件发送结束")
+            attachment.add_header('Content-Disposition', 'attachment', 
+                                filename=filepath.name)
+            self.message.attach(attachment)
+            logger.info(f"成功添加附件: {filepath.name}")
         except Exception as e:
-            log.error(e)
+            logger.error(f"添加附件失败: {str(e)}")
+        return self
+
+    def send(self) -> bool:
+        """
+        发送邮件
+        :return: 是否发送成功
+        """
+        try:
+            logger.info("开始发送邮件...")
+            self.smtp.ehlo()
+            if not self.use_ssl:
+                self.smtp.starttls()
+            self.smtp.login(self.sender, self.password)
+            self.smtp.send_message(self.message)
+            logger.info("邮件发送成功")
+            return True
+        except Exception as e:
+            logger.error(f"邮件发送失败: {str(e)}")
+            return False
         finally:
-            self.stp.quit()
+            try:
+                self.smtp.quit()
+            except Exception:
+                pass
 
 
 if __name__ == '__main__':
-    mail = SendEmail(receivers="9981@qq.com", theme="test", content="test email")
-    mail.set_file("../../resource/img/cat.png")
-    mail.send()
+    # 示例用法
+    email = SendEmail()
+    
+    email.set_receivers(["1678336204@qq.com"]) \
+        .set_subject("测试邮件") \
+        .set_content("<h1>这是一封测试邮件</h1>", "html") \
+        .add_attachment("../../resource/img/cat.png") \
+        .send()
