80a7f36c6e8a24af0b6327455c932f7918bb9e21
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2025-04-21 00:50:10 +0800

    更新

--- model/entity/selenium.py
+++ model/entity/selenium.py
@@ -11,15 +11,13 @@ class SeleniumBase(object):
     is_headless (bool, optional): 是否开启无头模式。默认为 False。
     is_cdp (bool, optional): 是否使用 Chrome Devtools Protocol。默认为 True。
     is_dev (bool, optional): 是否启用调试模式。默认为 True。
-    proxy (bool, optional): 代理设置。默认为 None。
-    proxy_value (str, optional): 代理的地址
+    proxy (str, optional): 代理设置。默认为 None，等于None则不适用代理。
     driver (str, optional): 浏览器驱动的路径。默认为 None。等于None会使用webdriver_manager自动下载驱动
     """
     is_headless: bool = False
     is_cdp: bool = True
     is_dev: bool = True
-    proxy: bool = False
-    proxy_value: str = None
+    proxy: str = None
     driver: str = None
 
 @dataclass
@@ -27,10 +25,10 @@ class LocateElementOptions(object):
     """
     定位元素选项
     wait (WebDriverWait): WebDriverWait 对象，用于显示等待元素出现
-    by: 元素定位方式（例如 By.ID，By.XPATH）。
-    value: 元素定位值（例如元素的 ID，XPath 表达式）。
-    method (str): 操作方法（默认为 None）。
-    key: 输入文本的值（默认为 None）。
+    by (str): 元素定位方式（例如 id，xpath）,必须输入的是字符类型。
+    value (str): 元素定位值（例如元素的 ID，XPath 表达式）。
+    method (LocateElementMethod): 操作方法（默认为 None）。
+    key (str): 输入文本的值（默认为 None）。
     is_more (bool): 是否返回多个元素，默认为 False。
     check_visibility (bool): 是否检查元素可见性（默认为 False）。
     """
--- test/page/base_page.py
+++ test/page/base_page.py
@@ -1,22 +1,22 @@
-from common.selenium.selenium_service import SeleniumService
+from common.selenium.selenium_service_normal import SeleniumServiceNormal
 from config.read_config import ReadConfig
 from model.entity.selenium import SeleniumBase
+from utils.time.time_utils import sleep
 
 
-class BasePage(SeleniumService):
+class BasePage(SeleniumServiceNormal):
     def __init__(self):
         super().__init__()
         config = SeleniumBase(
             is_headless=False,
             is_cdp=True,
             is_dev=True,
-            proxy=True,
-            proxy_value="127.0.0.1:7890",
+            proxy="127.0.0.1:7890",
         )
-        self.driver = SeleniumService()
+        self.driver = SeleniumServiceNormal()
         self.driver.start_browser(config)
         self.conf = ReadConfig()
 
     def open(self):
         self.driver.start_page(self.conf.url)
-        self.wait_for_time(2)
+        sleep()
